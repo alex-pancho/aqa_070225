3162cf2061629a979a07d894bca06912a36f7d3e
forfun <forfun@syneforge.com>
2025-02-20 20:29:03 +0200

    Внесені виправлення згідно коментаря в lms

--- lesson_03/homework_03.py
+++ lesson_03/homework_03.py
@@ -1,7 +1,10 @@
+import math
+
 # alice_in_wonderland = '"Would you tell me, please, which way I ought to go from here?"\n"That depends a good deal on where you want to get to," said the Cat.\n"I don't much care where ——" said Alice.\n"Then it doesn't matter which way you go," said the Cat.\n"—— so long as I get somewhere," Alice added as an explanation.\n"Oh, you're sure to do that," said the Cat, "if you only walk long enough."'
 # task 01 == Розділіть змінну alice_in_wonderland так, щоб вона займала декілька фізичних лінії
 # task 02 == Знайдіть та відобразіть всі символи одинарної лапки (') у тексті
 # task 03 == Виведіть змінну alice_in_wonderland на друк
+
 alice_in_wonderland = (
     '"Would you tell me, please, which way I ought to go from here?"\n'
     '"That depends a good deal on where you want to get to," said the Cat.\n'
@@ -24,11 +27,12 @@ print(alice_in_wonderland)
 моря становить 37 800 км2. Яку площу займають Чорне та Азов-
 ське моря разом?
 """
+# Дано
 square_black_sea = 436_402
 square_azov_sea = 37_800
 
+# Розв'язок
 total_square_sea = square_black_sea + square_azov_sea
-print(f"Чорне та Азовське моря разом займають {total_square_sea} км2")
 
 # task 05
 """
@@ -37,14 +41,15 @@ print(f"Чорне та Азовське моря разом займають {t
 250 449 товарів. На другому та третьому – 222 950 товарів.
 Знайдіть кількість товарів, що розміщені на кожному складі.
 """
+# Дано
 total_goods = 375_291
 first_second_goods = 250_449
 second_third_goods = 222_950
 
+# Розв'язок
 first_goods = total_goods - second_third_goods
 second_goods = first_second_goods - first_goods
 third_goods = total_goods - first_goods - second_goods
-print(f"На першому складі {first_goods} товарів, на другому {second_goods}, на третьому {third_goods}")
 
 # task 06
 """
@@ -53,11 +58,12 @@ print(f"На першому складі {first_goods} товарів, на др
 вати необхідно буде півтора року по 1179 грн/місяць. Обчисліть
 вартість комп’ютера.
 """
-mountly_payment = 1179
+# Дано
+mount_payment = 1179
 total_months = 12 * 1.5
 
-total_price = mountly_payment * total_months
-print(f"Вартість комп'ютера становить {total_price} грн")
+# Розв'язок
+total_price = mount_payment * total_months
 
 # task 07
 """
@@ -66,12 +72,13 @@ a) 8019 : 8     d) 7248 : 6
 b) 9907 : 9     e) 7128 : 5
 c) 2789 : 5     f) 19224 : 9
 """
-print(f"a) {8019 % 8}", end="     ")
-print(f"d) {7248 % 6}")
-print(f"b) {9907 % 9}", end="     ")
-print(f"e) {7128 % 5}")
-print(f"c) {2789 % 5}", end="     ")
-print(f"f) {19224 % 9}")
+# Дано
+numbers = [(8019, 8), (9907, 9), (2789, 5), (7248, 6), (7128, 5), (19224, 9)]
+
+# Розв'язок
+for dividend, divisor in numbers:
+    remainder = dividend % divisor
+    print(f"{dividend} : {divisor} -> остача {remainder}")
 
 # task 08
 """
@@ -85,23 +92,27 @@ print(f"f) {19224 % 9}")
 Торт            1           350 грн
 Вода            3           21 грн
 """
+# Дано
 pizza_large_price = 274
 pizza_medium_price = 218
 juice_price = 35
 cake_price = 350
 water_price = 21
+
 pizza_large_quantity = 4
 pizza_medium_quantity = 2
 juice_quantity = 4
 cake_quantity = 1
 water_quantity = 3
 
-total_order = (pizza_large_price * pizza_large_quantity +
-               pizza_medium_price * pizza_medium_quantity +
-               juice_price * juice_quantity +
-               cake_price * cake_quantity +
-               water_price * water_quantity)
-print(f"Іринці знадобиться {total_order} грн для замовлення")
+# Розв'язок
+total_pizza_large = pizza_large_price * pizza_large_quantity
+total_pizza_medium = pizza_medium_price * pizza_medium_quantity
+total_juice = juice_price * juice_quantity
+total_cake = cake_price * cake_quantity
+total_water = water_price * water_quantity
+
+total_order = total_pizza_large + total_pizza_medium + total_juice + total_cake + total_water
 
 # task 09
 """
@@ -110,14 +121,12 @@ print(f"Іринці знадобиться {total_order} грн для замо
 розміщено щонайбільше 8 фото. Скільки сторінок знадобиться
 Ігорю, щоб вклеїти всі фото?
 """
+# Дано
 photos = 232
 photos_per_page = 8
 
-if photos % photos_per_page == 0:
-    total_pages = photos // photos_per_page
-else:
-    total_pages = photos // photos_per_page + 1
-print(f"Ігорю знадобиться {total_pages} сторінок для вклеювання фото")
+# Розв'язок
+total_pages = math.ceil(photos / photos_per_page)
 
 # task 10
 """
@@ -130,14 +139,11 @@ print(f"Ігорю знадобиться {total_pages} сторінок для 
 равку під час цієї подорожі, кожного разу заправляючи пов-
 ний бак?
 """
+# Дано
 distance = 1600
 fuel_consumption = 9
 tank_capacity = 48
 
+# Розв'язок
 total_fuel = distance / 100 * fuel_consumption
-if total_fuel % tank_capacity == 0:
-    total_refills = total_fuel // tank_capacity
-else:
-    total_refills = total_fuel // tank_capacity + 1
-print(f"1) Для подорожі знадобиться {total_fuel} літрів бензину")
-print(f"2) Родині необхідно заїхати на заправку {total_refills} разів")
+total_refills = math.ceil(total_fuel / tank_capacity)
