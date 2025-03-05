31d50553cef97d74097b482f24591b459b96e513
iuliia_lazurkevych <lazurkevich@yahoo.com>
2025-03-05 19:27:37 +0200

    Completed homework#8

--- lesson_08/homework_08.py
+++ lesson_08/homework_08.py
@@ -18,9 +18,20 @@ sum_numbers_in_list("21")  # ValueError
 def sum_numbers_in_list(string_list: list):
     """Повертає список сум чисел зі списку строк,
     які складаються з чисел, розділених комою."""
-
-    
-    return
+    num_list = []
+    if not isinstance(string_list,list):
+        raise ValueError("List is expected as input")
+    if len(string_list) == 0:
+        raise ValueError("List is empty")
+    for i in string_list:
+        try:
+            numbers = map(int, i.split(','))
+            num_list.append(sum(numbers))
+        except ValueError:
+            num_list.append("Не можу це зробити!")
+        except AttributeError:
+            num_list.append("Не можу це зробити! AttributeError")
+    return num_list
 
 
 if __name__ == "__main__":
@@ -29,10 +40,9 @@ if __name__ == "__main__":
 
     output = sum_numbers_in_list(["1,2,3", "4/0,6", "asas7,8,9"])
     print(output)
-    """
-    sum_numbers_in_list(["1,2,3", "4,0,6"])  # [6, 10]
-    sum_numbers_in_list(["1,2,3", "asas7,8,9", "4,0,6"])  # [6, "Не можу це зробити!", 10]
-    sum_numbers_in_list(["1,2,3,4", 7])  # [10, "Не можу це зробити! AttributeError"]
-    sum_numbers_in_list([])  # ValueError
-    sum_numbers_in_list("21")  # ValueError
-    """
+
+# print(sum_numbers_in_list(["1,2,3", "4,0,6"]))  # [6, 10]
+# print(sum_numbers_in_list(["1,2,3", "asas7,8,9", "4,0,6"]))  # [6, "Не можу це зробити!", 10]
+# print(sum_numbers_in_list(["1,2,3,4", 7]))  # [10, "Не можу це зробити! AttributeError"]
+# print(sum_numbers_in_list([]))  # ValueError
+# print(sum_numbers_in_list("21"))  # ValueError
