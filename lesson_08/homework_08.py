24303743be54758f4ca0907946e83cfc25fd2615
NoTPr0 <vadeem.hacker@gmail.com>
2025-03-10 15:09:53 +0200

    Completed homework_08

--- lesson_08/homework_08.py
+++ lesson_08/homework_08.py
@@ -13,18 +13,24 @@ sum_numbers_in_list([])  # ValueError
 sum_numbers_in_list("21")  # ValueError
 ```
 """
+from os.path import split
 
 
 def sum_numbers_in_list(string_list: list):
     """Повертає список сум чисел зі списку строк,
     які складаються з чисел, розділених комою."""
     result = []
+
+    if not string_list or not isinstance(string_list, list):
+        raise ValueError()
     for item in string_list:
         try:
-            pass
+            result.append(sum(int(x) for x in item.split(",")))
+        except AttributeError as e:
+            result.append("Не можу це зробити! AttributeError")
         except ValueError as e:
             result.append("Не можу це зробити!")
-    
+
     return result
 
 
@@ -34,6 +40,7 @@ if __name__ == "__main__":
 
     output = sum_numbers_in_list(["1,2,3", "4/0,6", "asas7,8,9"])
     print(output)
+
     """
     sum_numbers_in_list(["1,2,3", "4,0,6"])  # [6, 10]
     sum_numbers_in_list(["1,2,3", "asas7,8,9", "4,0,6"])  # [6, "Не можу це зробити!", 10]
