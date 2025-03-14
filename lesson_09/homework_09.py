5ab85c26068938fb806d6de08e9e6262570cffe9
visla <visla@syneforge.com>
2025-03-14 16:17:59 +0200

    complete homework 09

--- lesson_09/homework_09.py
+++ lesson_09/homework_09.py
@@ -4,3 +4,31 @@
 який дозволяє змінювати середній бал студента. Виведіть інформацію про студента та змініть
 його середній бал.
 """
+
+
+class Student:
+    def __init__(self, first_name, last_name, age, avarage_point):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.avarage_point = avarage_point
+
+    def change_avarege(self, new_avarage_point):
+        self.avarage_point = new_avarage_point
+
+    def student_info(self):
+        print(f"Ім’я студента: {self.first_name}")
+        print(f"Прізвище студента: {self.last_name}")
+        print(f"Вік студента: {self.age}")
+        print(f"Середній бал студента: {self.avarage_point}")
+
+
+# Створили об’єкт Тетяна
+tetiana = Student("Tetiana", "Lypnyk", 30, "90")
+# вивели інформацю про тетяну
+tetiana.student_info()
+print("________________")
+# підвищили Тетяні бал
+tetiana.change_avarege("100")
+# показали нову інформацію про Тетяну
+tetiana.student_info()
