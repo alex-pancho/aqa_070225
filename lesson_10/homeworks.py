6e4f6dd09c77f7a317cf3acc0cd08598137655ce
iuliia_lazurkevych <lazurkevich@yahoo.com>
2025-03-16 22:13:06 +0200

    Completed homework #10

--- lesson_10/homeworks.py
+++ lesson_10/homeworks.py
@@ -1,2 +1,75 @@
-def func():
-    pass
+
+'''Рахування унікальних символів в строці
+ Порахувати кількість унікальних символів в строці. Якщо їх більше 10 - вивести в консоль True, 
+ інакше - False. Строку отримати за допомогою функції input()'''
+ 
+def count_unique_characters(str_for_count:str) -> bool:
+    """Functions count unique characters in string and:\n
+    - returns 'True' if string contains more than 10 unique characters;\n
+    - returns 'False' if string contains less than 10 unique characters.
+    """
+    if not isinstance (str_for_count, str):
+        raise TypeError('Only \'str\' type is expected.')
+    set_for_count = set(str_for_count)
+    set_len = len(set_for_count)
+    if set_len == 0:
+        raise ValueError('Empty string is entered.')
+    if set_len > 10:
+        return(True)
+    if set_len <= 10:
+        return(False)
+  
+""" Написати функцію, яка приймає два рядки та повертає індекс першого входження другого рядка
+у перший рядок, якщо другий рядок є підрядком першого рядка, та -1, якщо другий рядок
+не є підрядком першого рядка."""
+
+def find_substring(str1:str, str2:str) -> int:
+    """ Function checks if str1 contains str2.\n 
+    Returns index (position) of the first occurrence of str2 in str1.\n
+    Returns '-1' if str1 does not contain str2."""
+    if not isinstance (str1, str):
+        raise TypeError('Only \'str\' type is for str1.')
+    if not isinstance (str2, str):
+        raise TypeError('Only \'str\' type is for str2.')
+    if len(str1) < len(str2):
+        raise ValueError('str1 is expected to be longer than str2.')
+    if str2 in str1:
+        return(str1.index(str2))
+    else:
+        return(-1)
+ 
+"""  Задано список чисел numbers, потрібно знайти список квадратів
+парних чисел зі списку. Спробуйте використати if та цикл for в один рядок.
+"""
+def pow_even_number_list(number_list:list) -> list:
+    """Function returns list with power even numbers."""
+    if not isinstance(number_list, list):
+        raise TypeError('List type is expected.')
+    pow_number_list = [number**2 for number in number_list if number % 2 == 0]
+    return(pow_number_list)
+
+if __name__ == "__main__":
+
+# Func 1
+
+    str_for_count = 'qwerrrrrrrrttttttyyyyyyy'
+ 
+    print(f'Func 1: String contains more than 10 unique characters? Response: {count_unique_characters(str_for_count)}')
+
+# Func 2
+
+    str1 = "Hello, world!"
+    str2 = "world"
+
+    print(f'Func 2: Index of the first occurrence of str2 in str1 (-1 if str1 does not contain str2): {find_substring(str1, str2)}') # поверне 7
+    
+    str1 = "The quick brown fox jumps over the lazy dog"
+    str2 = "cat"
+
+    print(f'Func 2: Index of the first occurrence of str2 in str1 (-1 if str1 does not contain str2): {find_substring(str1, str2)}') # поверне -1
+
+# Func 3
+
+    number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    print(f'Func 3: New list with pow even numbers: {pow_even_number_list(number_list)}')  #  [4, 16, 36, 64, 100]
--- lesson_10/test_homework_10.py
+++ lesson_10/test_homework_10.py
@@ -7,15 +7,163 @@
 На оцінку впливає як якість тестів так і розмір тестового покриття. 
 Мінімум на 10 балів - 1 правильно задизайнений позитивний тест на функцію.
 """
-from homeworks import func
+from homeworks import count_unique_characters, find_substring, pow_even_number_list
 import unittest
 
-class NazvaTest(unittest.TestCase):
+class UniqueCharactersFunctionTest(unittest.TestCase):
 
-    def test_01(self):
-        """ Doc here """
-        pass
+    def test_count_unique_characters_01(self):
+        """Check if TRUE returned for > 10 unique characters."""
+        str_for_count = '123456789qw'
+        actual = count_unique_characters(str_for_count)
+        self.assertTrue(actual)
 
+    def test_count_unique_characters_02(self):
+        """Check if FALSE returned for <= 10 unique characters."""
+        str_for_count = '1234567890'
+        actual = count_unique_characters(str_for_count)
+        self.assertFalse(actual)
+
+    def test_count_unique_characters_03(self):
+        """Check if ValueError is raised for empty string input."""
+        str_for_count = ''
+        with self.assertRaises(ValueError):
+            count_unique_characters(str_for_count)
+
+    def test_count_unique_characters_04(self):
+        """Check if TypeError is raised when input is not \'str\' type."""
+        str_for_count = 0
+        with self.assertRaises(TypeError):
+            count_unique_characters(str_for_count)
+        str_for_count = [1, 2, 3]
+        with self.assertRaises(TypeError):
+            count_unique_characters(str_for_count)
+        str_for_count = (1, 2, 3)
+        with self.assertRaises(TypeError):
+            count_unique_characters(str_for_count)
+
+class FindSubstringFunctionTest(unittest.TestCase):
+
+    def test_find_substring_01(self):
+        """Check if '-1' is returned when str1 does not contain str2."""
+        str1 = "The quick brown fox jumps over the lazy dog"
+        str2 = "cat"
+        actual = find_substring(str1, str2)
+        expected = -1
+        self.assertEqual(actual, expected)
+
+    def test_find_substring_02(self):
+        """Check if index is returned if str1 contains str2."""
+        str1 = "Hello, world!"
+        str2 = "world"
+        actual = find_substring(str1, str2)
+        expected = 7
+        self.assertEqual(actual, expected)
+
+    def test_find_substring_03(self):
+        """Check if ValueError is raised when str2 is longer than str1."""
+        str1 = "world"
+        str2 = "Hello, world!"
+        with self.assertRaises(ValueError):
+            find_substring(str1, str2)
+
+    def test_find_substring_04(self):
+        """Check if TypeError is raised when str1 is not \'str\' type."""
+        str1 = 0
+        str2 = "Hello, world!"
+        with self.assertRaises(TypeError):
+            find_substring(str1, str2)
+        str1 = [1, 2, 3]
+        str2 = "Hello, world!"
+        with self.assertRaises(TypeError):
+            find_substring(str1, str2)
+        str1 = (1, 2, 3)
+        str2 = "Hello, world!"
+        with self.assertRaises(TypeError):
+            find_substring(str1, str2)
+        str1 = {1, 2, 3}
+        str2 = "Hello, world!"
+        with self.assertRaises(TypeError):
+            find_substring(str1, str2)
+
+    def test_find_substring_05(self):
+        """Check if TypeError is raised when str2 is not \'str\' type."""
+        str1 = "world"
+        str2 = 1.23
+        with self.assertRaises(TypeError):
+            find_substring(str1, str2)
+        str1 = "world"
+        str2 = [1, 2, 3]
+        with self.assertRaises(TypeError):
+            find_substring(str1, str2)
+        str1 = "world"
+        str2 = (1, 2, 3)
+        with self.assertRaises(TypeError):
+            find_substring(str1, str2)
+        str1 = "world"
+        str2 = {1, 2, 3}
+        with self.assertRaises(TypeError):
+            find_substring(str1, str2)
+
+    def test_find_substring_06(self):
+        """Check if '0' index is returned when str2 is empty """
+        str1 = "Hello, world!"
+        str2 = ""
+        actual = find_substring(str1, str2)
+        expected = 0
+        self.assertEqual(actual, expected)
+
+class PowEvenNumberListFunctionTest(unittest.TestCase):
+
+    def test_pow_even_number_list_01(self):
+         """Check for list with odd and even numbers"""
+         number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+         actual = pow_even_number_list(number_list)
+         expected = [4, 16, 36, 64, 100]
+         self.assertEqual(actual, expected)
+    
+    def test_pow_even_number_list_02(self):
+         """Check for list only with odd numbers"""
+         number_list = [1, 3, 5, 7, 9]
+         actual = pow_even_number_list(number_list)
+         expected = []
+         self.assertEqual(actual, expected)
+    
+    def test_pow_even_number_list_03(self):
+         """Check for empty list"""
+         number_list = []
+         actual = pow_even_number_list(number_list)
+         expected = []
+         self.assertEqual(actual, expected)
+    
+    def test_pow_even_number_list_04(self):
+         """Check for list with odd and even negative numbers"""
+         number_list = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
+         actual = pow_even_number_list(number_list)
+         expected = [4, 16, 36, 64, 100]
+         self.assertEqual(actual, expected)
+
+    def test_pow_even_number_list_05(self):
+         """Check if '0' is included in result with even numbers"""
+         number_list = [0, 2, 4, 6]
+         actual = pow_even_number_list(number_list)
+         expected = [0, 4, 16, 36]
+         self.assertEqual(actual, expected)
+
+    def test_pow_even_number_list_06(self):
+        """Check if TypeError is raised for non list type"""
+        number_list = 2
+        with self.assertRaises(TypeError):
+            pow_even_number_list(number_list)
+        number_list = "2"
+        with self.assertRaises(TypeError):
+            pow_even_number_list(number_list)
+        number_list = (0, 2, 4, 6)
+        with self.assertRaises(TypeError):
+            pow_even_number_list(number_list)
+        number_list = {0, 2, 4, 6}
+        with self.assertRaises(TypeError):
+            pow_even_number_list(number_list)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
