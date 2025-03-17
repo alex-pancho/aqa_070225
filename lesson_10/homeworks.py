f675da50d37df77e6be0edcca653354a8e3b2844
Lyubov Dmytriv <dmytriv.lyubov@gmail.com>
2025-03-17 23:12:46 +0200

    add homework_10

--- lesson_10/homeworks.py
+++ lesson_10/homeworks.py
@@ -1,2 +1,101 @@
-def func():
-    pass
+def sum_numbers_in_list(string_list: list):
+    '''Повертає список сум чисел зі списку строк,
+    які складаються з чисел, розділених комою.'''
+    result = []
+    #перевіряємо умову 5: якщо передано не список
+    if not isinstance(string_list, list) :
+        raise ValueError("Не список")
+    #перевіряємо умову 4: якщо передано порожній список
+    if len(string_list) == 0 :
+        raise ValueError("Список порожній")
+    # ітеруємо по елементах списку
+    for item in string_list:
+        #перевіряємо умову 3: якщо елемент списку не є рядком
+        if not isinstance(item, str):
+            result.append("Не можу це зробити! AttributeError")
+            #якщо елемент не рядок, переходимо до наступного елемента string_list
+            continue
+        else:
+            #якщо елемент рядок, робимо спробу виконати умову 1
+            try:
+                suma = 0
+                splitted_string = item.split(",")
+                for i in splitted_string:
+                    suma += float(i)
+                result.append(suma)
+            #Якщо рядок містить некоректні символи, виконуємться умова 2
+            except ValueError as e:
+                result.append("Не можу це зробити!")
+    return result
+
+class Student:
+    def __init__(self,first_name: str, last_name:str, age:float, gpa:float):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.gpa = gpa
+
+        #type/value check
+        if not isinstance(first_name, str):
+            raise ValueError("String is expected for first name") 
+        if not isinstance(last_name, str):
+            raise ValueError("String is expected for last name") 
+        if not isinstance(age, (int, float)) or not (16 <= age <= 100):
+            raise ValueError("Int or float in range 16-100 is expected for age") 
+        if not isinstance(gpa, (int, float)) or not (0 <= gpa <= 100):
+            raise ValueError("Int or float in range 0-100 is expected for GPA") 
+        
+         
+    def change_gpa(self, new_gpa:float):
+        """змінює середній бал студента"""
+        if isinstance(new_gpa, (int, float)) and (0 <= new_gpa <= 100):
+            self.gpa = new_gpa
+        else:
+            raise ValueError("Int or float in range 0-100 is expected for new GPA") 
+
+    def print_info(self):
+        print(f"Name: {self.first_name} {self.last_name} age: {self.age} gpa: {self.gpa}")
+
+            
+def average_of_list(list_of_numbers:list):
+    ''' Oбчислює середнє арифметичне списку чисел'''
+    #перевіряємо чи заданий список
+    if not isinstance(list_of_numbers, list):
+        raise ValueError("List is expected")
+    #перевіряємо чи всі елементи списку не числа
+    if all(not isinstance(list_element, (int, float)) for list_element in list_of_numbers):
+         raise ValueError("List has no number elements")
+    #перевіряємо чи список не порожній
+    if len(list_of_numbers) == 0:
+        raise ValueError("List is empty.At least one element is expected")   
+    #Якщо всі елемети числа, обчислюємо середнє арифметичне списку чисел
+    if all(isinstance(list_element, (int, float)) for list_element in list_of_numbers):
+        result = round(((sum(list_of_numbers)/len(list_of_numbers))), 3)
+        #Якщо є елемети числа та інші, обчислюємо середнє арифметичне списку чисел, пропускаємо елементи які не числа
+    else:
+        total_sum = 0
+        count = 0
+        for list_element in list_of_numbers:
+            if isinstance(list_element, (int, float)):
+                total_sum += list_element
+                count += 1
+        result = total_sum / count
+    return result
+
+
+
+        
+
+
+
+
+
+
+
+        
+
+        
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
+    #print(average_of_list([0, "0", 0]))
--- lesson_10/test_homework_10.py
+++ lesson_10/test_homework_10.py
@@ -7,14 +7,114 @@
 На оцінку впливає як якість тестів так і розмір тестового покриття. 
 Мінімум на 10 балів - 1 правильно задизайнений позитивний тест на функцію.
 """
-from homeworks import func
+from homeworks import sum_numbers_in_list
+from homeworks import Student
+from homeworks import average_of_list
 import unittest
 
-class NazvaTest(unittest.TestCase):
+class SumNumbersInList(unittest.TestCase):
+    def test_1_valid_input(self):
+        # перевіряє список сум чисел зі списку строк,які складаються з чисел, розділених комою
+        string_list = ["1,2,3", " 0,0", "-1, -4, 0.6" ]
+        actual = sum_numbers_in_list(string_list)
+        excepted = [6, 0, -4.4]
+        self.assertEqual(actual, excepted)
+        
+    def test_2_invalid_input_not_number_element(self):
+        #первіряє випадок коли рядковий елемент списоку містить не число
+        string_list = ["1,2,3", "4/0,6", "asas7,8,9", ""]
+        actual = sum_numbers_in_list(string_list)
+        excepted = [6, "Не можу це зробити!", "Не можу це зробити!", "Не можу це зробити!"]
+        self.assertEqual(actual, excepted)
+    
+    def test_3_invalid_input_not_str_element(self):
+        #первіряє випадок коли елемент списку не str
+        string_list = ["1,2,3,4", 7]
+        actual = sum_numbers_in_list(string_list)
+        excepted = [10, "Не можу це зробити! AttributeError"]
+        self.assertEqual(actual, excepted)
 
-    def test_01(self):
-        """ Doc here """
-        pass
+    def test_4_empty_list(self):
+        #первіряє випадок коли передано порожній список
+        string_list = []
+        with self.assertRaises(ValueError):
+            sum_numbers_in_list(string_list)
+
+    def test_5_not_list(self):
+        #первіряє випадок коли передано не список
+        string_list = "test"
+        with self.assertRaises(ValueError):
+            sum_numbers_in_list(string_list)    
+
+class ChangeGpa(unittest.TestCase):
+    def test_1_valid_input_gpa_float(self):
+        #перевірка зміни середнього балу, якщо бал задовільняє умову float in range 0-100
+        student_1 = Student("John","Dou", 18, 85.5)
+        student_1.change_gpa(81.5)
+        actual = student_1.gpa
+        excepted = 81.5
+        self.assertEqual(actual, excepted)
+
+    def test_2_valid_input_gpa_int(self):
+        #перевірка зміни середнього балу, якщо бал задовільняє умову int in range 0-100
+        student_1 = Student("John","Dou", 18, 90)
+        student_1.change_gpa(89)
+        actual = student_1.gpa
+        excepted = 89
+        self.assertEqual(actual, excepted)  
+
+    def test_3_invalid_input_gpa_not_number(self):
+        #перевірка випадку виклику помилки якщо середній бал не число
+        student_1 = Student("John","Dou", 18, 90)
+        with self.assertRaises(ValueError):
+            student_1.change_gpa({89})
+       
+
+    def test_4_invalid_input_gpa_below_zero(self):
+        #перевірка випадку виклику помилки якщо середній бал < 0
+        student_1 = Student("John","Dou", 18, 90)
+        with self.assertRaises(ValueError):
+            student_1.change_gpa(-1)
+        
+    def test_5_invalid_input_gpa_over_100(self):
+        #перевірка випадку виклику помилки якщо середній бал >100
+        student_1 = Student("John","Dou", 18, 90)
+        with self.assertRaises(ValueError):
+            student_1.change_gpa(101)
+            
+
+class AvarageOfList(unittest.TestCase):
+    def test_01_valid_input(self):
+        #перевіряє середнє арифметичне списку чисел
+        my_list = [3, 2, 3, 4]
+        actual = average_of_list(my_list)
+        expected = 3
+        self.assertEqual(actual, expected)
+
+    def test_02_mixed_input_number_and_not_number(self):
+        #перевіряє середнє арифметичне списку де крім чисел є інші типи
+        my_list = [1, "a", -1, [1], 6]
+        actual = average_of_list(my_list)
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_03_invalid_input_all_elements_not_number(self):
+        #перевіряє випадок коли передано список всі елементи не числа
+        my_list = ["a", [1],{1}]
+        with self.assertRaises(ValueError):
+            average_of_list(my_list)
+
+    def test_04_invalid_input_empty_list(self):
+        #первіряє випадок коли передано порожній список
+        my_list = []
+        with self.assertRaises(ValueError):
+            average_of_list(my_list)
+
+    def test_05_invalid_input_not_list(self):
+        #первіряє чи випадок коли передано не список
+        my_list = ("a", [1],{1})
+        with self.assertRaises(ValueError):
+            average_of_list(my_list)
 
 
 if __name__ == "__main__":
