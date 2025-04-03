845b2f30cffa076a5ce15d35d4dd9486e4bbb899
Ilona <ilonabenedek841@gmail.com>
2025-04-03 21:12:03 +0200

    Added homework14

--- lesson_14/homework_14.py
+++ lesson_14/homework_14.py
@@ -13,3 +13,50 @@
     значення кут_б обчислюється автоматично.
     Для встановлення значень атрибутів використовуйте метод __setattr__.
 """
+
+class Romb:
+    """
+This code validates and sets the value of angle_a.
+It first checks whether the input value is within the valid range (0 to 180).
+If not, it raises a ValueError to prevent invalid geometry.
+If the value is valid, it sets angle_a using super().__setattr__.
+Then, it automatically calculates the adjacent angle (angle_b)
+as 180 minus angle_a, and sets it as well.
+This ensures that the sum of two adjacent angles in a rhombus
+is always 180 degrees.
+    """
+
+    def __init__(self, side_length, angle_a):
+        self.side_length = side_length
+        self.angle_a = angle_a
+
+    def __setattr__(self, name, value):
+        if name == "side_length":
+            if value <= 0:
+                raise ValueError("Side length must be greater than 0.")
+            super().__setattr__(name, value)
+
+        elif name == "angle_a":
+            if value <= 0 or value >= 180:
+               raise ValueError("Angle must be between 0 and 180 degrees.")
+            super().__setattr__("angle_a", value)
+            super().__setattr__("angle_b", 180 - value)
+
+        elif name == "angle_b":
+            raise AttributeError("angle_b is calculated automatically and cannot be set manually.")
+
+        else:
+            super().__setattr__(name, value)
+
+    def __repr__(self):
+        return f"Romb(side_length={self.side_length}, angle_a={self.angle_a}, angle_b={self.angle_b})"
+
+if __name__ == "__main__":
+    my_romb = Romb(12, 75)
+    print(my_romb)
+
+    another_romb = Romb(9.5, 65)
+    print(another_romb)
+ 
+
+        
