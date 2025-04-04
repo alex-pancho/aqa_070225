407530c436fc805e26407a0208437e10d4b1431e
visla <visla@syneforge.com>
2025-04-04 13:48:55 +0300

    complete homework 14

--- lesson_14/homework_14.py
+++ lesson_14/homework_14.py
@@ -13,3 +13,43 @@
     значення кут_б обчислюється автоматично.
     Для встановлення значень атрибутів використовуйте метод __setattr__.
 """
+
+
+class Rhombus:
+    def __init__(self, side_a, angle_a):
+
+        self.side_a = side_a
+        self.angle_a = angle_a  # це автоматично встановлює і кут angle_b
+
+    @property
+    def side_a(self):
+        return self._side_a
+
+    @side_a.setter
+    def side_a(self, value):
+        if value <= 0:
+            raise ValueError("Значення сторони сторона_а повинно бути більше 0.")
+        self._side_a = value
+
+    @property
+    def angle_a(self):
+        return self._angle_a
+
+    @angle_a.setter
+    def angle_a(self, value):
+        if not (0 < value < 180):
+            raise ValueError("Кут повинен бути в межах від 0 до 180 градусів і кути повинні задовольняти умову: кут_а "
+                             "+ кут_б = 180")
+        self._angle_a = value
+
+    @property
+    def angle_b(self):
+        return 180 - self._angle_a
+
+    def __repr__(self):
+        return f"Ромб: (сторона_а={self.side_a}, кут_a={self.angle_a}, кут_б={self.angle_b})"
+
+
+if __name__ == "__main__":
+    romb = Rhombus(side_a=22, angle_a=22)
+    print(romb)
