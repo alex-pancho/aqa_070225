e488a6dbcd5cef3904acd17f204500180217a817
NoTPr0 <vadeem.hacker@gmail.com>
2025-06-11 22:16:36 +0300

    Completed homework_28

--- lesson_28/pages/garage_page.py
+++ lesson_28/pages/garage_page.py
@@ -2,10 +2,10 @@ from pages.base_page import BasePage
 
 
 class GaragePage(BasePage):
-    
+
     def __init__(self, driver):
         super().__init__(driver)
-    
+
     add_car = '//button[@class="btn btn-primary"]'
     brand = '//*[@id="addCarBrand"]'
     model = '',  # TODO: add the xpath here
@@ -13,3 +13,7 @@ class GaragePage(BasePage):
     add = '//button[. = "Add"]'
     new_car = '//li[@class="car-item"]'
     guest_btn = '//button[. = "Guest log in"]'
+    settings = '//a[@routerlink="settings"]'
+    remove_account = '//button[contains(text(), "Remove my account")]'
+    remove_confirm = '//button[text() = "Remove"]'
+    profile='//a[@routerlink="profile"]'
---
+++ lesson_28/tests/test_homework_28.py
@@ -0,0 +1,5 @@
+def test_01_registration_by_sign_up(registration_flow, delete_account):
+    pass
+
+
+
