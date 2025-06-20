0b6bdd1781a003bf807540e25990cc010d6cfe7f
dor.vit <stegmat@meta.ua>
2025-06-20 04:06:48 +0300

    homework_24

--- lesson_24/conftest.py
+++ lesson_24/conftest.py
@@ -1,49 +1,57 @@
+"""Setup for tests."""
+
+import logging
+from logging.handlers import RotatingFileHandler
+
 import pytest
 import requests
+from requests.auth import HTTPBasicAuth
 
-# Визначення фікстури з допомогою декоратора @pytest.fixture
-@pytest.fixture
-def my_fix():
-    # Ця частина коду буде виконана перед кожним тестом,
-    # який використовує цю фікстуру
-    data = {"key": "value"}
-    yield data
+base_url = 'http://127.0.0.1:8080'
+username = 'DorVit'
+password = 'test_123'
 
-@pytest.fixture(scope="function", autouse=True)
-def my_printable_fixture(request):
-    print(f"Test {request.node.name}")
-    yield # test itself run
-    print("Test end message")
+@pytest.fixture(scope='session', autouse=True)
+def test_logger():
+    """Logger setup."""
+    logger = logging.getLogger('Main')
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-@pytest.fixture(params=[1, 2, 3])
-def my_fixture(request):
-    param_value = request.param
-    print(f"Setup with param value: {param_value}")
-    return param_value * 2
+    # Console settings
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
 
-# Параметризована фікстура
-@pytest.fixture(params=[requests.get, requests.post])
-def http_method(request):
-    return request.param
+    # Rotating file handler settings
+    rotating_handler = RotatingFileHandler(filename='test_search.log', mode='a', maxBytes=10000, backupCount=5)
+    rotating_handler.setLevel(logging.INFO)
+    rotating_handler.setFormatter(formatter)
 
-@pytest.fixture(scope='class') # module
-def prepare_database(): 
-    print("Підготовка бази даних...")
-    yield
-    print("Очищення бази даних...")
+    logger.handlers = [console_handler, rotating_handler]
 
+    return logger
 
-@pytest.fixture(scope='module', autouse=True)
-def prepare_config():
-    print("Підготовка конфігурації...")
-    yield
-    print("Очищення конфігурації...")
+@pytest.fixture(scope='class')
+def auth_process():
+    """Fixture to authenticate and provide access."""
+    session = None
+    try:
+        # Auth request
+        response = requests.post(f'{base_url}/auth', auth=HTTPBasicAuth(username, password), timeout=5)
+        response.raise_for_status()
 
-@pytest.fixture(params=[(1, 2), (5, 5), (10, -5)])
-def input_data(request):
-    return request.param
+        # Token response
+        token = response.json()['access_token']
+        session = requests.Session()
+        session.headers.update({'Authorization': f'Bearer {token}'})
 
-@pytest.fixture
-def prepare_data(request):
-    data = request.param * 2
-    return data
+        yield session
+
+    except requests.exceptions.RequestException as err:
+        pytest.fail(f"Request error occurred: {err}", pytrace=True)
+    except Exception as exc:
+        pytest.fail(f"Unexpected error occurred: {exc}", pytrace=True)
+    finally:
+        if session is not None:
+            session.close()
